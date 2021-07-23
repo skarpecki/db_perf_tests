fb85204365f6421158e2230cd84119f4c14692ed
skarpecki <skarpecki@users.noreply.github.com>
2021-07-24 01:28:35 +0200

    Poprawiony blad dla 3 joinow

--- sql_server/skrypty/sql/selects/three_joins_where.sql
+++ sql_server/skrypty/sql/selects/three_joins_where.sql
@@ -4,5 +4,5 @@ INNER JOIN perf_tests.dbo.companies c ON e.company_id = c.company_id
 INNER JOIN perf_tests.dbo.departments d ON e.department_id = d.department_id
 WHERE  e.first_name='Jeff'
 	AND a.city = 'ELGIN' 
-	AND c.sector = 'Pharma';
+	AND c.sector = 'Pharma'
 	AND d.name = 'Marketing';
