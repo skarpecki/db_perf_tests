b7c2c3f1cb8c6c45af268e5c10d84ac698f868e6
skarpecki <skarpecki@users.noreply.github.com>
2021-07-26 18:01:27 +0200

    Zaktualizowany skrypt bash indeksujacy clients i cities

--- sql_server/skrypty/bash/cities_indexing.sh
+++ sql_server/skrypty/bash/cities_indexing.sh
@@ -2,5 +2,5 @@
 
 mkdir -p /results/indexing
 ( time /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD \
--Q "CREATE NONCLUSTERED INDEX IX_addresses ON perf_tests.dbo.addresses (city)" ) \
+-Q "CREATE NONCLUSTERED INDEX IX_cities ON perf_tests.dbo.cities (city)" ) \
 |& tee /results/indexing/addresses.txt
