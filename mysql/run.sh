8e218af0070a11d5b97ef6f696aad8cdad0076d0
Szymon <szymon.karpecki@gmail.com>
2021-08-05 00:07:57 +0200

    Zmiana budowania i uruchamiania kontenera dockerowe

--- mysql/run.sh
+++ mysql/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo docker run --name="mysql_srvr" --cpus=1 -p 3306:3306 --memory="8G" -v mysql-vol:/var/lib/mysql -d mysql_srvr
+sudo docker run --name="mysql_srvr" --cpus=1 -p 3306:3306 --memory="8G" -v mysql_vol:/var/lib/mysql -d mysql_srvr
