1891f6d537e51b7b5b80d42f72efe6414db1e42e
skarpecki <skarpecki@users.noreply.github.com>
2021-08-05 23:39:48 +0200

    MSSQL - poprawa struktury

--- sql_server/skrypty/bash/employees_deleting.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-mkdir -p /results/deleting/$1
-( time /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -Q "DELETE FROM perf_tests.dbo.employees WHERE employee_id=1000001;" > /dev/null ) |& tee /results/deleting/$1/employees.txt
--- sql_server/skrypty/bash/employees_inserting.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-mkdir -p /results/inserting/$1
-( time /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -Q "INSERT INTO perf_tests.dbo.employees SELECT 1000001, 'John', 'Wick', 'john@wick.com', 1, 1, 1;" > /dev/null ) |& tee /results/inserting/$1/employees.txt
