0334b37e303adf2f89c36d8ab52492c605a37093
Szymon <szymon.karpecki@gmail.com>
2021-08-07 13:14:12 +0200

    MySQL dodanie uprawnienia do wykonania

--- mysql/skrypty/bash/timing/select_timing_all.sh
+++ mysql/skrypty/bash/timing/select_timing_all.sh
@@ -2,22 +2,22 @@
 
 count=6
 
-../selecting/select_all.sh "clients" "before_index" $count
-../selecting/clients_indexing.sh
-../selecting/select_all.sh "clients" "after_index" $count
+/skrypty/bash/selecting/select_all.sh "clients" "before_index" $count
+/skrypty/bash/indexing/clients_indexing.sh
+/skrypty/bash/select_all.sh "clients" "after_index" $count
 
-../selecting/select_all.sh "cities" "before_index" $count
-../selecting/cities_indexing.sh
-../selecting/select_all.sh "cities" "after_index" $count
+/skrypty/bash/select_all.sh "cities" "before_index" $count
+/skrypty/bash/indexing/cities_indexing.sh
+/skrypty/bash/selecting/select_all.sh "cities" "after_index" $count
 
-../selecting/select_all.sh "companies" "before_index" $count
-../selecting/companies_indexing.sh
-../selecting/select_all.sh "companies" "after_index" $count
+/skrypty/bash/selecting/select_all.sh "companies" "before_index" $count
+/skrypty/bash/indexing/companies_indexing.sh
+/skrypty/bash/selecting/select_all.sh "companies" "after_index" $count
 
-../selecting/select_all.sh "departments" "before_index" $count
-../selecting/departments_indexing.sh
-../selecting/select_all.sh "departments" "after_index" $count
+/skrypty/bash/selecting/select_all.sh "departments" "before_index" $count
+/skrypty/bash/indexing/departments_indexing.sh
+/skrypty/bash/selecting/select_all.sh "departments" "after_index" $count
 
-../selecting/select_all.sh "orders" "before_index" $count
-../selecting/orders_indexing.sh
-../selecting/select_all.sh "orders" "after_index" $count
+/skrypty/bash/selecting/select_all.sh "orders" "before_index" $count
+/skrypty/bash/indexing/orders_indexing.sh
+/skrypty/bash/selecting/select_all.sh "orders" "after_index" $count
