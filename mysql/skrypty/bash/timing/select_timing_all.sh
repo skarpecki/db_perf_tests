d1afdaa8601cc34355cfa2372d32bf6363268adf
Szymon <szymon.karpecki@gmail.com>
2021-08-11 22:23:02 +0200

    MySQL Poprawka timing all

--- mysql/skrypty/bash/timing/select_timing_all.sh
+++ mysql/skrypty/bash/timing/select_timing_all.sh
@@ -4,9 +4,9 @@ count=6
 
 /skrypty/bash/selecting/select_all.sh "clients" "before_index" $count
 /skrypty/bash/indexing/clients_indexing.sh
-/skrypty/bash/select_all.sh "clients" "after_index" $count
+/skrypty/bash/selecting/select_all.sh "clients" "after_index" $count
 
-/skrypty/bash/select_all.sh "cities" "before_index" $count
+/skrypty/bash/selecting/select_all.sh "cities" "before_index" $count
 /skrypty/bash/indexing/cities_indexing.sh
 /skrypty/bash/selecting/select_all.sh "cities" "after_index" $count
 
@@ -20,4 +20,4 @@ count=6
 
 /skrypty/bash/selecting/select_all.sh "orders" "before_index" $count
 /skrypty/bash/indexing/orders_indexing.sh
-/skrypty/bash/selecting/select_all.sh "orders" "after_index" $count
+/skrypty/bash/selecting/select_all.sh "orders" "after_index" $count]
