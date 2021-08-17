c3dc43536ffaf8f1666ec1d0ec5bd7c12be14ee7
skarpecki <skarpecki@users.noreply.github.com>
2021-08-17 22:23:08 +0200

    [Oracle] Utworzony skrypt tworzący tabele.

---
+++ oracle/skrypty/sql/create_all.sql
@@ -0,0 +1,53 @@
+ALTER SESSION SET CONTAINER = ORCLPDB1;
+
+DECLARE
+user_exist integer;
+BEGIN
+  SELECT count(*) INTO user_exist FROM dba_users WHERE username='PERF_TESTS';
+  IF (user_exist = 0) THEN
+    EXECUTE IMMEDIATE 'CREATE USER PERF_TESTS IDENTIFIED BY password';
+  END IF;
+END;
+
+ALTER SESSION SET CURRENT_SCHEMA = PERF_TESTS;
+
+
+
+CREATE TABLE clients (
+	client_id int PRIMARY KEY,
+	first_name varchar(50),
+	last_name varchar(50),
+	email varchar(50),
+	city_id int,
+	department_id int,
+	company_id int
+);
+
+
+CREATE TABLE cities (
+	city_id int PRIMARY KEY,
+	country varchar(50),
+	city varchar(50),
+	postal_code varchar(50)
+);
+
+
+CREATE TABLE companies (
+	company_id int PRIMARY KEY,
+	name varchar(50),
+	sector varchar(50),
+	legal_form varchar(50)
+);
+
+
+CREATE TABLE departments (
+	department_id int PRIMARY KEY,
+	name varchar(50)
+);
+
+
+CREATE TABLE orders (
+	oreder_id int PRIMARY KEY,
+	part_num varchar(50),
+	client_id int
+);
