126646028b7cf8238462f23517b4692b2aa26e65
skarpecki <skarpecki@users.noreply.github.com>
2021-08-24 21:48:21 +0200

    [MySQL] Poprawa komendy indeksujacej

--- mysql/skrypty/log/bash/clients_indexing.sh
+++ mysql/skrypty/log/bash/clients_indexing.sh
@@ -2,13 +2,13 @@
 
 mkdir -p /results/indexing
 ( time mysql -u 'root' -p$MYSQL_ROOT_PASSWORD \
--e "CREATE NONCLUSTERED INDEX IX_clients ON perf_tests.clients (first_name, last_name)" ) \
+-e "CREATE INDEX IX_clients ON perf_tests.clients (first_name, last_name);" ) \
 |& tee /results/indexing/clients.txt
 
 for i in $(seq 0 6)
 do
     val=$(( 10 ** i ))
     ( time mysql -u 'root' -p$MYSQL_ROOT_PASSWORD \
-    -e "CREATE NONCLUSTERED INDEX IX_clients_$val ON perf_tests.clients_$val (first_name, last_name)" ) \
+    -e "CREATE INDEX IX_clients_$val ON perf_tests.clients_$val (first_name, last_name);" ) \
     |& tee /results/indexing/clients_$val.txt
 done
