897cc6058ccd41b14df328245ce7de916514356e
Szymon <szymon.karpecki@gmail.com>
2021-08-24 20:51:59 +0200

    [MySQL] Poprawa pojedynczych cudzyslowiow na podwojne

--- mysql/skrypty/log/bash/imports.sh
+++ mysql/skrypty/log/bash/imports.sh
@@ -5,5 +5,5 @@ for script in /skrypty/log/sql/imports/*.sql
 do
 script_name=$(basename $script .sql)
 mkdir -p /results/imports
-    ( time mysql -u 'root' -p$MYSQL_ROOT_PASSWORD < $script > /dev/null ) |& tee /results/imports/$script_name.txt
+    ( time mysql -u "root" -p$MYSQL_ROOT_PASSWORD < $script > /dev/null ) |& tee /results/imports/$script_name.txt
 done
