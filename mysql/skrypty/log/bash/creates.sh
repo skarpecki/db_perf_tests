9a5afcc3e1d53aae6f45ae75d67e1a7ab99ad7bd
skarpecki <skarpecki@users.noreply.github.com>
2021-08-24 20:15:06 +0200

    [MySQL] Skrypty do badania logarytmicznie zmienianego rozmiaru

---
+++ mysql/skrypty/log/bash/creates.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+mkdir -p /results/creates/
+mysql -u 'root' -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS perf_tests;" > /dev/null
+( mysql -u 'root' -p$MYSQL_ROOT_PASSWORD < /skrypty/log/sql/create_all.sql  > /dev/null ) |& tee /results/creates/creates.txt
---
+++ mysql/skrypty/log/sql/selects/clients_100000_where.sql
@@ -0,0 +1,2 @@
+SELECT * FROM perf_tests.clients_100000
+WHERE first_name = 'Amanda' AND last_name='Carter'; 
