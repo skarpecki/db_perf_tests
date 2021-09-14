e3883d95d2e9b29e9f04d52be85b54e4428da561
Szymon Karpęcki <szymon.karpecki@dataedo.com>
2021-09-14 15:29:29 +0200

    [MSSQL] Popawione indeksowanie

--- mssql/skrypty/insert/bash/clients_indexing.sh
+++ mssql/skrypty/insert/bash/clients_indexing.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 mkdir -p /results/indexing
-( time /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD \
+(   /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P  $SA_PASSWORD \
 -Q "CREATE NONCLUSTERED INDEX IX_clients ON perf_tests.dbo.clients (first_name, last_name);" ) \
 |& tee /results/indexing/clients.txt
+
