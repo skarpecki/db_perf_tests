2f59c434540d8fdeb3516a18fa590732a82d70ac
dev <szymon.karpecki@gmail.com>
2021-09-19 18:51:40 +0000

    [Oracle] Dodanie skryptow administrujacych kontener

---
+++ oracle/prune.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker rm -f oracle_srvr
+docker rm -f oracle_vol
--- oracle/run.sh
+++ oracle/run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-DIRECTORY=`dirname $0`
-DIRECTORY=$(realpath $DIRECTORY)
+DIRECTORY=$(realpath $(dirname $0))
 
 sudo docker run --name="oracle_srvr" --cpus=1 --memory="8G" -p 1521:1521 -p 5500:5500 -v oracle_vol:/ORCL -v ${DIRECTORY}/sql:/docker-entrypoint-initdb.d/setup -d oracle_srvr
