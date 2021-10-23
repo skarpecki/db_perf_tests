5e99f6b17b4db18e931a1b039cf266d91bf731ff
skarpecki <skarpecki@users.noreply.github.com>
2021-10-23 22:49:35 +0200

    [MSSQL] Poprawione skrypty select dla window function

--- mssql/skrypty/window_function/sql/selects/employees_1000_all.sql
+++ mssql/skrypty/window_function/sql/selects/employees_1000_all.sql
@@ -3,10 +3,9 @@ SELECT e.first_name
         ,e.email
         ,d.name
         ,e.salary
-        ,first_value(e.salary) OVER w AS max_salary_in_dept
-        ,e.salary / (first_value(e.salary) OVER w) * 100 AS percentage_of_max_salary
-FROM 
-perf_tests.employees_1000 e
-INNER JOIN perf_tests.departments d
-	ON e.department_id = d.department_id
-WINDOW w AS (PARTITION BY d.name ORDER BY salary DESC);
+        ,first_value(e.salary) OVER (PARTITION BY d.name ORDER BY salary DESC) AS max_salary_in_dept
+        ,e.salary / (first_value(e.salary) OVER (PARTITION BY d.name ORDER BY salary DESC)) * 100 AS percentage_of_max_salary
+FROM
+perf_tests.dbo.employees_1000 e
+INNER JOIN perf_tests.dbo.departments d
+	ON e.department_id = d.department_id;
