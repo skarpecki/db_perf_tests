a567f0871b881102675dcd08b50cde39d1fcf564
skarpecki <skarpecki@users.noreply.github.com>
2021-10-24 12:26:48 +0200

    [Oracle] Dodane skrypty select

--- oracle/skrypty/window_function/sql/selects/clients_1000_all.sql
+++
@@ -1,6 +0,0 @@
-SELECT  first_name,
-        last_name,
-        count(*) as ppl_count
-FROM perf_tests.clients_1000
-GROUP BY first_name, last_name
-ORDER BY ppl_count DESC;
---
+++ oracle/skrypty/window_function/sql/selects/employees_100_all.sql
@@ -0,0 +1,11 @@
+SELECT e.first_name
+		,e.last_name
+        ,e.email
+        ,d.name
+        ,e.salary
+        ,first_value(e.salary) OVER (PARTITION BY d.name ORDER BY salary DESC) AS max_salary_in_dept
+        ,e.salary / (first_value(e.salary) OVER (PARTITION BY d.name ORDER BY salary DESC)) * 100 AS percentage_of_max_salary
+FROM 
+perf_tests.employees_100 e
+INNER JOIN perf_tests.departments d
+	ON e.department_id = d.department_id;
