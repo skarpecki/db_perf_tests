33592a19462fda7fba050454b1d24792fafa66a8
skarpecki <skarpecki@users.noreply.github.com>
2021-11-04 23:02:59 +0100

    [MSSQL] Poprawka buga

--- mssql/skrypty/group_by_order/bash/clients_indexing.sh
+++ mssql/skrypty/group_by_order/bash/clients_indexing.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD \
--Q "CREATE NONCLUSTERED INDEX IX_clients ON perf_tests.dbo.clients (first_name, last_name);" ) 
+-Q "CREATE NONCLUSTERED INDEX IX_clients ON perf_tests.dbo.clients (first_name, last_name);"
 
 for i in $(seq 0 6)
 do
